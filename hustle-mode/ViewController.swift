3a84399dccfdc400cce5c3180d7da03b6456c0ac
Mark Carruthers <markpc17@sky.com>
2018-06-04 23:43:45 +0100

    Adjusted Y axis

--- hustle-mode/ViewController.swift
+++ hustle-mode/ViewController.swift
@@ -42,7 +42,7 @@ class ViewController: UIViewController {
         player.play()
         
         UIView.animate(withDuration: 2.3, animations: {
-            self.rocket.frame = CGRect(x: 0, y: 20, width: 375, height: 484)
+            self.rocket.frame = CGRect(x: 0, y: 140, width: 375, height: 484)
         }) { (finished) in
             self.hustleLbl.isHidden = false
             self.onLbl.isHidden = false
